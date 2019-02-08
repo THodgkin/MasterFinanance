7d63dd3adf5a17f96fb84a40d0166beef282bb1e
Tyler Hodgkin <tjh2389@hotmail.com>
2019-02-07 23:15:52 -0700

    Configuration Form Code

---
+++ MasterFinananceUI/Forms/frmConfiguration.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace MasterFinananceUI.Forms
+{
+    partial class frmConfiguration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.lblServer = new System.Windows.Forms.Label();
+            this.btnServerConnect = new System.Windows.Forms.Button();
+            this.lblDatabase = new System.Windows.Forms.Label();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.btnCreateDB = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loadingCircle = new MRG.Controls.UI.LoadingCircle();
+            this.btnSelectDB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(12, 29);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(229, 25);
+            this.txtServer.TabIndex = 0;
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Location = new System.Drawing.Point(9, 9);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(87, 17);
+            this.lblServer.TabIndex = 1;
+            this.lblServer.Text = "Server Name";
+            // 
+            // btnServerConnect
+            // 
+            this.btnServerConnect.Location = new System.Drawing.Point(247, 29);
+            this.btnServerConnect.Name = "btnServerConnect";
+            this.btnServerConnect.Size = new System.Drawing.Size(138, 25);
+            this.btnServerConnect.TabIndex = 2;
+            this.btnServerConnect.Text = "Connect to Server";
+            this.btnServerConnect.UseVisualStyleBackColor = true;
+            this.btnServerConnect.Click += new System.EventHandler(this.btnServerConnect_Click);
+            // 
+            // lblDatabase
+            // 
+            this.lblDatabase.AutoSize = true;
+            this.lblDatabase.Location = new System.Drawing.Point(9, 64);
+            this.lblDatabase.Name = "lblDatabase";
+            this.lblDatabase.Size = new System.Drawing.Size(214, 17);
+            this.lblDatabase.TabIndex = 4;
+            this.lblDatabase.Text = "Choose a name for your database";
+            this.lblDatabase.Visible = false;
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(12, 85);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(229, 25);
+            this.txtDatabase.TabIndex = 5;
+            this.txtDatabase.Visible = false;
+            this.txtDatabase.TextChanged += new System.EventHandler(this.txtDatabase_TextChanged);
+            // 
+            // btnCreateDB
+            // 
+            this.btnCreateDB.Location = new System.Drawing.Point(247, 85);
+            this.btnCreateDB.Name = "btnCreateDB";
+            this.btnCreateDB.Size = new System.Drawing.Size(138, 25);
+            this.btnCreateDB.TabIndex = 6;
+            this.btnCreateDB.Text = "Create Database";
+            this.btnCreateDB.UseVisualStyleBackColor = true;
+            this.btnCreateDB.Visible = false;
+            this.btnCreateDB.Click += new System.EventHandler(this.btnCreateDB_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            // 
+            // loadingCircle
+            // 
+            this.loadingCircle.Active = false;
+            this.loadingCircle.Color = System.Drawing.Color.DarkGray;
+            this.loadingCircle.InnerCircleRadius = 6;
+            this.loadingCircle.Location = new System.Drawing.Point(392, 30);
+            this.loadingCircle.Name = "loadingCircle";
+            this.loadingCircle.NumberSpoke = 9;
+            this.loadingCircle.OuterCircleRadius = 7;
+            this.loadingCircle.RotationSpeed = 100;
+            this.loadingCircle.Size = new System.Drawing.Size(27, 24);
+            this.loadingCircle.SpokeThickness = 4;
+            this.loadingCircle.StylePreset = MRG.Controls.UI.LoadingCircle.StylePresets.Firefox;
+            this.loadingCircle.TabIndex = 8;
+            this.loadingCircle.Text = "loadingCircle1";
+            this.loadingCircle.Visible = false;
+            // 
+            // btnSelectDB
+            // 
+            this.btnSelectDB.Location = new System.Drawing.Point(247, 85);
+            this.btnSelectDB.Name = "btnSelectDB";
+            this.btnSelectDB.Size = new System.Drawing.Size(138, 25);
+            this.btnSelectDB.TabIndex = 9;
+            this.btnSelectDB.Text = "Select Database";
+            this.btnSelectDB.UseVisualStyleBackColor = true;
+            this.btnSelectDB.Visible = false;
+            this.btnSelectDB.Click += new System.EventHandler(this.btnSelectDB_Click);
+            // 
+            // frmConfiguration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 189);
+            this.Controls.Add(this.btnSelectDB);
+            this.Controls.Add(this.loadingCircle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCreateDB);
+            this.Controls.Add(this.txtDatabase);
+            this.Controls.Add(this.lblDatabase);
+            this.Controls.Add(this.btnServerConnect);
+            this.Controls.Add(this.lblServer);
+            this.Controls.Add(this.txtServer);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmConfiguration";
+            this.Text = "Configuration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.Button btnServerConnect;
+        private System.Windows.Forms.Label lblDatabase;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.Button btnCreateDB;
+        private System.Windows.Forms.Label label1;
+        private MRG.Controls.UI.LoadingCircle loadingCircle;
+        private System.Windows.Forms.Button btnSelectDB;
+    }
+}
---
+++ MasterFinananceUI/Forms/frmConfiguration.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace MasterFinananceUI.Forms
+{
+    public partial class frmConfiguration : Form
+    {
+        public frmConfiguration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnServerConnect_Click(object sender, EventArgs e)
+        {
+            loadingCircle.Active = true;
+            loadingCircle.Visible = true;
+            string server = txtServer.Text;
+            string connString = String.Format("Data Source={0};Initial Catalog=master;Integrated Security=True;", server);
+
+            Properties.Settings.Default.connectionString = connString;
+            Properties.Settings.Default.Save();
+
+
+            if (IsValidConnectionString(connString) == true)
+            {
+                this.btnServerConnect.Visible = false;
+                this.txtServer.Enabled = false;
+                this.lblDatabase.Visible = true;
+                this.txtDatabase.Visible = true;
+            }
+            loadingCircle.Active = false;
+            loadingCircle.Visible = false;
+        }
+
+        private bool IsValidConnectionString(string connString)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                string connectionStatus = conn.State.ToString();
+
+                if (connectionStatus == "Open")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        private void btnCreateDB_Click(object sender, EventArgs e)
+        {
+
+            string server = txtServer.Text;
+            string dataBase = txtDatabase.Text;
+            string connString = Properties.Settings.Default.connectionString;
+            string query = String.Format("CREATE DATABASE {0}", dataBase);
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            connString = String.Format("Data Source={0};Initial Catalog={1};Integrated Security=True;", server, dataBase);
+
+            Properties.Settings.Default.connectionString = connString;
+            Properties.Settings.Default.Save();
+            txtDatabase.Enabled = false;
+            btnCreateDB.Visible = false;
+            btnSelectDB.Visible = false;
+            label1.Visible = true;
+            label1.Text = String.Format("You have created your database named {0}", dataBase);
+        }
+        private void btnSelectDB_Click(object sender, EventArgs e)
+        {
+            string server = txtServer.Text;
+            string dataBase = txtDatabase.Text;
+            string connString = Properties.Settings.Default.connectionString;
+            string query = String.Format("CREATE DATABASE {0}", dataBase);
+
+            connString = String.Format("Data Source={0};Initial Catalog={1};Integrated Security=True;", server, dataBase);
+
+            Properties.Settings.Default.connectionString = connString;
+            Properties.Settings.Default.Save();
+            txtDatabase.Enabled = false;
+            btnCreateDB.Visible = false;
+            btnSelectDB.Visible = false;
+            label1.Visible = true;
+            label1.Text = String.Format("You have sucessfully connected to {0}", dataBase);
+
+        }
+        private void txtDatabase_TextChanged(object sender, EventArgs e)
+        {
+            string dbName = txtDatabase.Text;
+            string query = "SELECT COUNT(*) FROM sys.databases DBs WHERE DBs.name = @database";
+            string connString = Properties.Settings.Default.connectionString;
+
+            if (dbName.Length > 0)
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    conn.Open();
+                    cmd.Parameters.AddWithValue("@database", dbName);
+                    int rowCount = Convert.ToInt32(cmd.ExecuteScalar());
+                    if (rowCount >= 1)
+                    {
+                        btnCreateDB.Visible = false;
+                        btnSelectDB.Visible = true;
+                    }
+                    else
+                    {
+                        btnCreateDB.Visible = true;
+                        btnSelectDB.Visible = false;
+                    }
+                }
+            }
+            else
+            {
+                btnCreateDB.Visible = false;
+                btnSelectDB.Visible = false;
+            }
+        }
+    }
+}
